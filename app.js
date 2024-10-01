91c4590d2cce598808c8462954653d9959ac34d7
zu0910 <zullyortiz.09@gmail.com>
2024-10-01 12:53:59 -0500

    feat :sparkles: advances of filtro

--- app.js
+++ app.js
@@ -1,74 +1,170 @@
+function Dashboard(){
+    document.getElementById("main").innerHTML=`
+    <h1 class="title">Dashboard</h1>
+    <ul class="breadcrumbs">
+        <li><a href="#">Home</a></li>
+        <li class="divider">/</li>
+        <li><a href="#" class="active">Dashboard</a></li>
+    </ul>
+    <div id="buscador">
+        <input class="form-control me-2" type="text" id="enterId" placeholder="Search" onkeyup="show()">
+    </div>
+    
+    <div class="info-data">
+        
+    </div>
+    `
+}
 
 function show(){
-    var buscar = document.getElementById("enterId").value
+    var buscar = document.getElementById("enterId").value;
     var url = "https://imdb.iamidiotareyoutoo.com/search?q=" + buscar ;
-    var busqueda = url+"true beauty";
-    function buscar_id(url){
-        fetch(busqueda)
-        .then( res => res.json())
-        .then( data => {
-            console.log(data);
-            
-            data.description.forEach(element => {
-                
-                document.querySelector(".info-data").innerHTML+=`
-                <div class="card">
-					<div class="head">
-						<div>
-							<h2>${element["#TITLE"]}</h2>
-                        </div>
-                    </div>
-                    <img class="photo" src="${element["#IMG_POSTER"]}">
-                    <div class="contenido">
-                        <p>Year: ${element["#YEAR"]}</p>
-					    <p>Actors: ${element["#ACTORS"]}</p>
-                        <p>${element["#AKA"]}</p>
-                    </div>
-				</div>
-                `
-                
-            });
-            
-        })
-    }
-    buscar_id(busqueda)
+    console.log(buscar);
     
-
+    
+    fetch(url)
+    .then( res => res.json())
+    .then( data => {
+        document.querySelector(".info-data").innerHTML=""
+        data.description.forEach(element => {
+            document.querySelector(".info-data").innerHTML+=`
+            <div class="card">
+                <div class="head">
+                    <div>
+                        <h2>${element["#TITLE"]}</h2>
+                    </div>
+                </div>
+                <img class="photo" src="${element["#IMG_POSTER"]}">
+                <div class="contenido">
+                    <p>Year: ${element["#YEAR"]}</p>
+                    <p>Actors: ${element["#ACTORS"]}</p>
+                    <p>${element["#AKA"]}</p>
+                </div>
+            </div>
+            `
+        });
+    })
+    
 }
 show();
 
-// function search_Id(){
-//     let search = document.getElementById("enterId").value;
-//     let link = "https://imdb.iamidiotareyoutoo.com/search?q="+search;
-//     show(link)
-// }
 
-// function show(link){
+function año(){
+    let buscar = document.getElementById("enterId");
+    buscar.setAttribute('placeholder', 'Ingrese el año');
+    buscar.setAttribute('onkeyup', mostrar_año()) ;
+    buscar.value="";
+    document.querySelector(".info-data").innerHTML=""
+}
+año();
+function mostrar_año(){
+    var buscar = document.getElementById("enterId").value;
+    var url = "https://imdb.iamidiotareyoutoo.com/search?q=" + buscar ;
+    console.log(buscar);
+    
+    
+    fetch(url)
+    .then( res => res.json())
+    .then( data => {
+        document.querySelector(".info-data").innerHTML=""
+        data.description.forEach(element => {
+            document.querySelector(".info-data").innerHTML+=`
+            <div class="card">
+                <div class="head">
+                    <div>
+                        <h2>${element["#TITLE"]}</h2>
+                    </div>
+                </div>
+                <img class="photo" src="${element["#IMG_POSTER"]}">
+                <div class="contenido">
+                    <p>Year: ${element["#YEAR"]}</p>
+                    <p>Actors: ${element["#ACTORS"]}</p>
+                    <p>${element["#AKA"]}</p>
+                </div>
+            </div>
+            `
+        });
+    })
+}
+mostrar_año();
 
-//     fetch(link)
-//         .then( res => res.json())
-//         .then( data => {
-//             console.log(data);
-            
-//             data.description.forEach(element => {
-                
-//                 document.querySelector(".info-data").innerHTML+=`
-//                 <div class="card">
-//                     <div class="head">
-//                         <div>
-//                             <h2>${element["#TITLE"]}</h2>
-//                         </div>
-//                     </div>
-//                     <img class="photo" src="${element["#IMG_POSTER"]}">
-//                     <div class="contenido">
-//                         <p>Year: ${element["#YEAR"]}</p>
-//                         <p>Actors: ${element["#ACTORS"]}</p>
-//                         <p>${element["#AKA"]}</p>
-//                     </div>
-//                 </div>
-//                 `
-                
-//             });
-            
-//         })
-// }
+function actor(){
+    let buscar = document.getElementById("enterId");
+    buscar.setAttribute('placeholder', 'Ingrese el nombre del actor ');
+    buscar.setAttribute('onkeyup', mostrar_actor()) ;
+    buscar.value="";
+    document.querySelector(".info-data").innerHTML=""
+}
+actor();
+function mostrar_actor(){
+    var buscar = document.getElementById("enterId").value;
+    var url = "https://imdb.iamidiotareyoutoo.com/search?q=" + buscar ;
+    console.log(buscar);
+    
+    
+    fetch(url)
+    .then( res => res.json())
+    .then( data => {
+        document.querySelector(".info-data").innerHTML=""
+        data.description.forEach(element => {
+            document.querySelector(".info-data").innerHTML+=`
+            <div class="card">
+                <div class="head">
+                    <div>
+                        <h2>${element["#TITLE"]}</h2>
+                    </div>
+                </div>
+                <img class="photo" src="${element["#IMG_POSTER"]}">
+                <div class="contenido">
+                    <p>Year: ${element["#YEAR"]}</p>
+                    <p>Actors: ${element["#ACTORS"]}</p>
+                    <p>${element["#AKA"]}</p>
+                </div>
+            </div>
+            `
+        });
+    })
+}
+mostrar_actor();
+
+function imdb(){
+    let buscar = document.getElementById("enterId");
+    buscar.setAttribute('placeholder', 'Ingrese el IMDB');
+    buscar.setAttribute('onkeyup', mostrar_imdb()) ;
+    buscar.value="";
+    document.querySelector(".info-data").innerHTML=""
+}
+imdb();
+function mostrar_imdb(){
+    var buscar = document.getElementById("enterId").value;
+    var url = "https://imdb.iamidiotareyoutoo.com/search?q=" + buscar ;
+    console.log(buscar);
+    
+    
+    fetch(url)
+    .then( res => res.json())
+    .then( data => {
+        document.querySelector(".info-data").innerHTML=""
+        data.description.forEach(element => {
+            document.querySelector(".info-data").innerHTML+=`
+            <div class="card">
+                <div class="head">
+                    <div>
+                        <h2>${element["#TITLE"]}</h2>
+                    </div>
+                </div>
+                <img class="photo" src="${element["#IMG_POSTER"]}">
+                <div class="contenido">
+                    <p>Year: ${element["#YEAR"]}</p>
+                    <p>Actors: ${element["#ACTORS"]}</p>
+                    <p>${element["#AKA"]}</p>
+                </div>
+            </div>
+            `
+        });
+    })
+}
+mostrar_imdb();
+
+
+
